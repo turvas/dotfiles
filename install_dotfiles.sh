c0eb8f8d21666e6dddd523055fa067a8e9610332
alex [dot] kramer [at] g_m_a_i_l [dot] com <mitochondrion@users.noreply.github.com>
2019-08-01 16:41:28 -0400

    Update install_dotfiles.sh

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -21,4 +21,4 @@ ln -s ${PWD}/.ctags ~/.ctags
 # Install Git completion from the official Git repo
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
 
-source ~/.bash_profile
+echo -e "Run the following command to complete installation:\nsource ~/.bash_profile"
