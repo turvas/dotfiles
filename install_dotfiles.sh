606625648b8e0bf87a0b69678f82fd167aefe1ad
mitochondrion <mitochondrion@users.noreply.github.com>
2020-01-16 10:33:37 +0200

    Saving files before refreshing line endings

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -13,14 +13,14 @@ mv ~/.bash_profile ~/.bash_profile.$NOW
 mv ~/.vimrc ~/.vimrc.$NOW
 mv ~/.gitconfig ~/.gitconfig.$NOW
 mv ~/.gitignore ~/.gitignore.$NOW
-mv ~/.ctags ~/.ctags.$NOW
+#mv ~/.ctags ~/.ctags.$NOW
 
 # Create symlinks to repository dotfiles
-ln -s ${PWD}/.bash_profile ~/.bash_profile
-ln -s ${PWD}/.vimrc ~/.vimrc
-ln -s ${PWD}/.gitconfig ~/.gitconfig
-ln -s ${PWD}/.gitignore ~/.gitignore
-ln -s ${PWD}/.ctags ~/.ctags
+ln -sn ${PWD}/.bash_profile ~/.bash_profile
+ln -sn ${PWD}/.vimrc ~/.vimrc
+ln -sn ${PWD}/.gitconfig ~/.gitconfig
+ln -sn ${PWD}/.gitignore ~/.gitignore
+#ln -sn ${PWD}/.ctags ~/.ctags
 
 # Install Git completion from the official Git repo
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
