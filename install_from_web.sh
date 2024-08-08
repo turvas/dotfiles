550891e533dea1ea12f60745dd58af48d587ba40
Kalev Kuusik <kalev.kuusik@bytelife.com>
2024-08-08 14:26:55 +0300

    added option to use alternative git repo for install_from_web.sh

--- install_from_web.sh
+++ install_from_web.sh
@@ -2,6 +2,7 @@
 # meant for onliner install, like
 # curl -L https://github.com/turvas/dotfiles/raw/master/install_from_web.sh | sh
 #
+set -e
 which git > /dev/null
 if [ $? -gt 0 ]; then # missing
 	OS=`uname | awk '{print $1}'`
@@ -18,7 +19,12 @@ if [ $? -gt 0 ]; then # missing
                 sudo yum install -y git
         fi
 fi
-git clone https://turvas@github.com/turvas/dotfiles.git ~/.dotfiles
+if [ -z "$1" ]; then
+	GIT_REPO=turvas@github.com/turvas
+else
+	GIT_REPO=$1
+fi
+git clone https://$GIT_REPO/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
 ./install_dotfiles.sh # Don't worry, this will preserve existing dotfiles with timestamps!
 source ~/.bash_profile # Apply new dotfiles to current shell
